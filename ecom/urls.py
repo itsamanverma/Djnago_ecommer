b205860604e8e6f58982f64f08a947e196fcc017
itsamanverma <amanvermame786@gmail.com>
2022-06-20 21:48:23 +0530

    pre-populate the column value & setup the sqlite-studio client to see the data of table

--- ecom/urls.py
+++ ecom/urls.py
@@ -21,8 +21,9 @@ from django.urls import path
 from ecom.settings import MEDIA_ROOT
 from . import views
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-]+static(settings.MEDIA_URL,document_root=MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
