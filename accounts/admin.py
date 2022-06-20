63cea541f4fd75d491eb0d0c9dbc54212b6eb580
itsamanverma <amanvermame786@gmail.com>
2022-06-20 19:58:31 +0530

    manipulation with the django administartion user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,18 @@
 import imp
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('email','username','first_name','last_name','last_login','date_joined','is_active')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    list_display_links = ('email','first_name','last_name')
+    readonly_fields = ('last_login','date_joined')
+    ordering = ('-date_joined',)
+
+admin.site.register(Account,AccountAdmin)
