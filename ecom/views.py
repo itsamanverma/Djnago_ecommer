d5e4d4f2d071e9b4436f7cbbeab5c258483fdb77
itsamanverma <amanvermame786@gmail.com>
2022-06-21 23:12:48 +0530

    add products base on the category

--- ecom/views.py
+++ ecom/views.py
@@ -1,4 +1,7 @@
+from itertools import product
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
+    products = Product.objects.all().filter(is_available=True)
     return render(request, 'home.html')
