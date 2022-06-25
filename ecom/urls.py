3b1add2224eac9d42ffc8ff4ec0f6bb2a2112d43
itsamanverma <amanvermame786@gmail.com>
2022-06-25 19:20:19 +0530

    add store.html & store app

--- ecom/urls.py
+++ ecom/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django import views
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from ecom.settings import MEDIA_ROOT
 from . import views
@@ -26,4 +27,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    path('store/',include('store.urls',)),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
