df56f2a40a97529046a7bcce6e1695899f7ec488
itsamanverma <amanvermame786@gmail.com>
2022-06-25 23:09:57 +0530

    display the product to store page by category

--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
+from category.models import Category
+
 
 # Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     context = {
         'products': products,
