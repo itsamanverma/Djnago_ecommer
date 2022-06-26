5fdd4a49a6baa74b36fad7bde98c0c0f37214f1b
itsamanverma <amanvermame786@gmail.com>
2022-06-27 00:22:28 +0530

    product detail page url

--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from builtins import Exception
+from ast import Try
 from django.shortcuts import get_object_or_404, render
 from .models import Product
 from category.models import Category
@@ -21,3 +23,14 @@ def store(request, category_slug=None):
         'product_count': product_count
     }
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug=None, product_slug=None):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+    except Exception as e:
+        raise e
+        
+    context = {
+        'single_product': single_product
+    }
+    return render(request, 'store/product_detail.html',context)
