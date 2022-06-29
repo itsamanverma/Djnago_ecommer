9bed2f07b9d1602b2b3775d79ddf56036b6cbbfa
itsamanverma <amanvermame786@gmail.com>
2022-06-29 18:51:54 +0530

    having some issue try to fixed'

--- carts/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('',views.cart, name='cart'),
-    path('add_cart/<int:product_id>/',views.add_cart, name='add_cart'),  
-]
--- ecom/settings.py
+++ ecom/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
-    'carts'
 ]
 
 MIDDLEWARE = [
