6b51ebd3c1fcaf53016df5088f18f9b41360752d
itsamanverma <amanvermame786@gmail.com>
2022-07-01 11:08:25 +0530

    add functinality if item is already added & view cart option

--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from builtins import Exception
 from ast import Try
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+
+from carts.models import CartItem
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
 
 
 # Create your views here.
@@ -27,10 +31,14 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug=None, product_slug=None):
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(product=single_product, cart__cart_id=_cart_id(request)).exists() # check if the product is in the cart or not if it is then return true otherwise return false
+        # return HttpResponse(in_cart) # return the in_cart variable as a json object 
+        # exit()
     except Exception as e:
-        raise e
+        raise e # raise the exception
         
     context = {
-        'single_product': single_product
+        'single_product': single_product, # get the product by slug from the database
+        'in_cart': in_cart  # return the in_cart variable as a json object 
     }
     return render(request, 'store/product_detail.html',context)
